d5e34dd5e07ffcbdc1fae9a40e9c1694bba7921f
JYZ-zhao <541311283@qq.com>
2018-06-25 09:32:32 +0800

    spring-boot第一天代码 helloWorld

--- src/main/java/com/zjy/springboot01helloworld/contorller/HelloController.java
+++ src/main/java/com/zjy/springboot01helloworld/contorller/HelloController.java
@@ -3,11 +3,15 @@ package com.zjy.springboot01helloworld.contorller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//这个注解表示这个类所有方法返回的数据直接写给浏览器(如果是对象转成json)
+/*@Controller
+@ResponseBody*/
+@RestController
 public class HelloController {
 
-    @ResponseBody
+    //@ResponseBody
     @RequestMapping("/hello")
     public String hello() {
         return "helloWorld";
